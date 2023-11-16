e14eb8dad53e90aa129a94aac4498dc08110a309
Iffat Hossain <iffatshaon@gmail.com>
2023-11-17 00:35:17 +0600

    Updated Captcha code

--- functions/Captcha.py
+++ functions/Captcha.py
@@ -1,14 +1,25 @@
-from captcha.image import ImageCaptcha
-from io import BytesIO
 from fastapi.responses import StreamingResponse
+from io import BytesIO
+from captcha.image import ImageCaptcha
+import random
+import string
 
-def getCaptcha():
-    image = ImageCaptcha(width = 250, height = 90, fonts=['assets/fonts/grafity.ttf','assets/fonts/aachen.ttf'])
-    captcha_text = 'abcde' 
-    data = image.generate(captcha_text)
+def generate_text_and_hash():
+    characters = string.ascii_letters + string.digits
+    captcha_text = ''.join(random.choice(characters) for _ in range(5))
+    hash_value = random.getrandbits(128)
+    return captcha_text, hash_value
+
+def generate_captcha_image(text):
+    image = ImageCaptcha(width=250, height=90, fonts=['assets/fonts/roman.ttf'])
+    data = image.generate(text)
     captcha_image = BytesIO(data.read())
     captcha_image.seek(0)
-    return StreamingResponse(captcha_image, media_type="image/png")
+    return captcha_image
 
-def checkCaptcha():
-    pass
+def getCaptcha():
+    captcha_text, hash_value = generate_text_and_hash()
+    captcha_image = generate_captcha_image(captcha_text)
+    response_data = {"hash_value": hash_value}
+    captcha_image.seek(0)
+    return StreamingResponse(captcha_image, media_type="image/png", headers={"data": str(response_data)})
